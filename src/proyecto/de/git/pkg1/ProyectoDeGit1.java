5eb391fa545bd0972031bc8bcc489ccd844872d4
Valentina Roldán <valentina.roldan07@outlook.com>
2024-07-09 07:26:26 -0600

    creando repositorio

---
+++ src/proyecto/de/git/pkg1/ProyectoDeGit1.java
@@ -0,0 +1,14 @@
+
+package proyecto.de.git.pkg1;
+
+public class ProyectoDeGit1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Mi proyecto");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
